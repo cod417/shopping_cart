e1a2144d4915f4b007a0f5cc03cfc3b47ff1e04e
Balbir <programmingwithmosh@gmail.com>
2022-04-22 22:03:59 +0530

    deplyed

---
+++ src/data.js
@@ -0,0 +1,31 @@
+const data = {
+    products: [
+      {
+        id: '1',
+        name: 'MacBook',
+        price: 1400,
+        image: 'https://picsum.photos/id/180/2400/1600',
+      },
+      {
+        id: '2',
+        name: 'Old Car',
+        price: 2400,
+        image: 'https://picsum.photos/id/111/4400/2656',
+      },
+      {
+        id: '3',
+        name: 'laptop -Access',
+        price: 1000,
+        image: 'https://wwwcdn.cincopa.com/_cms/design18/images/HTMLPhotogallery.jpg?fts=2018-07-25T07:52:05.3458266Z',
+  
+      },
+      {
+        id: '4',
+        name: 'Gadgets',
+        price: 1000,
+        image: 'https://picsum.photos/id/119/3264/2176'
+      },
+    ],
+  };
+  export default data;
+  
